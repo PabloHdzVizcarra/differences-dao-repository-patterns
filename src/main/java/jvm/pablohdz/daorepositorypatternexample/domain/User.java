812c00076e0ee311e05cfafbc863811e636d8bc0
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-29 13:17:22 -0500

    feat create entity user

---
+++ src/main/java/jvm/pablohdz/daorepositorypatternexample/domain/User.java
@@ -0,0 +1,67 @@
+package jvm.pablohdz.daorepositorypatternexample.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_username")
+    private String username;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_email")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
