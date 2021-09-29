3224c0221e6b5c2aee5e27b2d64f1509ecb376cc
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-29 13:05:17 -0500

    first commit

---
+++ src/test/java/jvm/pablohdz/daorepositorypatternexample/DaoRepositoryPatternExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jvm.pablohdz.daorepositorypatternexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoRepositoryPatternExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
