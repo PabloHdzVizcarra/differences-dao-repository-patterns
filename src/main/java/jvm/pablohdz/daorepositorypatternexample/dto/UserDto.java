9b25d7fd75002fbc99ea51dee00ce1f413e95bb1
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-29 14:24:47 -0500

    feat create dto to response to the client

---
+++ src/main/java/jvm/pablohdz/daorepositorypatternexample/dto/UserDto.java
@@ -0,0 +1,41 @@
+package jvm.pablohdz.daorepositorypatternexample.dto;
+
+public class UserDto {
+    private long id;
+    private String username;
+    private String name;
+    private String email;
+
+    public UserDto(long id, String username, String name, String email) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
