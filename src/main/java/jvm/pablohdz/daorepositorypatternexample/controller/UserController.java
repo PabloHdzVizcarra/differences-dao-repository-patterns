8ca41a4e8b701f88204533130f64034f20bb3c59
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-29 19:04:15 -0500

    feat controller can save user

--- src/main/java/jvm/pablohdz/daorepositorypatternexample/controller/UserController.java
+++ src/main/java/jvm/pablohdz/daorepositorypatternexample/controller/UserController.java
@@ -1,12 +1,16 @@
 package jvm.pablohdz.daorepositorypatternexample.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import jvm.pablohdz.daorepositorypatternexample.dto.UserDto;
+import jvm.pablohdz.daorepositorypatternexample.dto.UserRequest;
 import jvm.pablohdz.daorepositorypatternexample.service.UserService;
 
 @RestController
@@ -23,5 +27,11 @@ public class UserController {
     public ResponseEntity<List<UserDto>> fetchUsers() {
         List<UserDto> listUsers = userService.fetchUsers();
         return ResponseEntity.ok(listUsers);
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> save(@RequestBody UserRequest user) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.save(user));
     }
 }
