f1b3596e7e7039207f3964f02d51e8d9e3676232
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-01 20:23:08 -0500

    feat create method to save a tweet

--- src/main/java/jvm/pablohdz/daorepositorypatternexample/persistence/TweetDatabase.java
+++ src/main/java/jvm/pablohdz/daorepositorypatternexample/persistence/TweetDatabase.java
@@ -2,7 +2,12 @@ package jvm.pablohdz.daorepositorypatternexample.persistence;
 
 import java.util.List;
 
+import jvm.pablohdz.daorepositorypatternexample.dto.TweetDto;
+import jvm.pablohdz.daorepositorypatternexample.dto.TweetRequest;
+
 public interface TweetDatabase<T> {
 
     List<T> findByEmail(String email);
+
+    TweetDto saveData(TweetRequest tweetRequest);
 }
