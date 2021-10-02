196ef7be672e31fe2a3a96207dad54ad9267bf77
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-01 20:32:18 -0500

    fix change return method

--- src/main/java/jvm/pablohdz/daorepositorypatternexample/persistence/TweetDatabase.java
+++ src/main/java/jvm/pablohdz/daorepositorypatternexample/persistence/TweetDatabase.java
@@ -2,12 +2,11 @@ package jvm.pablohdz.daorepositorypatternexample.persistence;
 
 import java.util.List;
 
-import jvm.pablohdz.daorepositorypatternexample.dto.TweetDto;
 import jvm.pablohdz.daorepositorypatternexample.dto.TweetRequest;
 
 public interface TweetDatabase<T> {
 
     List<T> findByEmail(String email);
 
-    TweetDto saveData(TweetRequest tweetRequest);
+    long saveData(TweetRequest tweetRequest);
 }
